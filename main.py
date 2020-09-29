6b7af6a0f82a22033f33fa8edf24bfcae23632c1
Suhaas B <suhaasbmk@outlook.com>
2020-09-29 18:47:01 +0000

    added 1st line of story

--- main.py
+++ main.py
@@ -3,15 +3,26 @@ name = input("What is your name? \n")
 print("Hello,", name)
 age = int(input("What is your age? \n"))
 
-is_older = age >= 13 and age <= 80
-print(is_older)
-
 if age >=13 and age <= 80:
   print("Great!")
+
+  cont = input("Do you want to continue(y/N)? \n").lower()
+  if cont == "y":
+    print("\n...Entering Dungeon...\n")
+
+    ch1 = input("Do you want to go left(a) or right(b)?\n")
+    if ch1 == 'a':
+      ch2 = input("You followed a clear path and found the gatekeeper to the City of Holkn. Do you wish to talk(a) or sneak in(b)?\n")
+
+    elif ch == 'b':
+      ch2 = input("You found a back alley door to the City of Holkn. You sneak in and you find a drunk watching you. Do you ignore(a) or talk(b)?\n")
+
+    else :
+      print("You have chosen death.")
+
 else:
   print("Oops you need to be 13 or older.")
 
-
-print("\n\nThank you for playing!")
+print("\n\nGame still in development, Thank you for playing!")
 
 
