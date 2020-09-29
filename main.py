dfd35ae0ea2db5058b1fba9ceaa4e56030d1934b
Suhaas B <suhaasbmk@outlook.com>
2020-09-29 18:23:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+print("Welcome to my first game!")
+name = input("What is your name? \n")
+print("Hello,", name)
+age = int(input("What is your age? \n"))
+
+is_older = age >= 13 and age <= 80
+print(is_older)
+
+if age >=13 and age <= 80:
+  print("Great!")
+else:
+  print("Oops you need to be 13 or older.")
+
+
+print("\n\nThank you for playing!")
+
+
